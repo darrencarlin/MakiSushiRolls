db4d14a5378b82b025613dba226745280ab98050
darrencarlin <dazftw@gmail.com>
2018-03-16 20:36:14 +0000

    test

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    
+    
+    let burger = document.querySelector(".burger")
+
+    burger.addEventListener("click", function () {
+        this.classList.toggle("open");
+    });   
+  
+
+
+
+});
